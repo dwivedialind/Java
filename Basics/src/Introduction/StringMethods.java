33accc717432df6eaf335f2991f31dded33daf34
dwivedialind <84173548+dwivedialind@users.noreply.github.com>
2024-02-02 12:23:03 +0530

    add comment

--- Basics/src/Introduction/StringMethods.java
+++ Basics/src/Introduction/StringMethods.java
@@ -4,6 +4,7 @@ package Introduction;
 public class StringMethods {
     public static void main(String[] args) {
         String message = "Hello \"World\"";
+        //String are immutable
         System.out.println(message.toLowerCase());
         System.out.println(message);
     }
