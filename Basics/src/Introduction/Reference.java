143e5f71a8a3dd5284f0b98f4688497d49101944
dwivedialind <84173548+dwivedialind@users.noreply.github.com>
2024-02-02 11:44:12 +0530

    Reference type

---
+++ Basics/src/Introduction/Reference.java
@@ -0,0 +1,11 @@
+package Introduction;
+
+import java.util.Date;
+
+public class Reference {
+    public static void main(String[] args) {
+        long code = 123_123_33_3333L;
+        Date today = new Date();
+        System.out.println(today);
+    }
+}
