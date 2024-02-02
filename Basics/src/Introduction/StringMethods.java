f71684ab2358f2589396ee97f3fb8160ae46dd94
dwivedialind <84173548+dwivedialind@users.noreply.github.com>
2024-02-02 12:13:00 +0530

    String and escape sequence

---
+++ Basics/src/Introduction/StringMethods.java
@@ -0,0 +1,10 @@
+package Introduction;
+
+
+public class StringMethods {
+    public static void main(String[] args) {
+        String message = "Hello \"World\"";
+        System.out.println(message.toLowerCase());
+        System.out.println(message);
+    }
+}
